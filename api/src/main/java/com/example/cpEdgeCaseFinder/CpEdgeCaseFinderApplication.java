5d49497eaa0d4aebc878d16b44a432e427726308
deepakr2k1 <dev.deepakr2k1@gmail.com>
2024-04-18 02:24:49 +0200

    updated app name

--- api/src/main/java/com/example/cpEdgeCaseFinder/CpEdgeCaseFinderApplication.java
+++ api/src/main/java/com/example/cpEdgeCaseFinder/CpEdgeCaseFinderApplication.java
@@ -1,13 +1,13 @@
-package com.example.cpdebuggerbackend;
+package com.example.cpEdgeCaseFinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CpDebuggerBackendApplication {
+public class CpEdgeCaseFinderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CpDebuggerBackendApplication.class, args);
+		SpringApplication.run(CpEdgeCaseFinderApplication.class, args);
 	}
 
 }
